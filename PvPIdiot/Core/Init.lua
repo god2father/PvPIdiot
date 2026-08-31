0e8f0a654d9e24f3edee05b2556e634b15a8c036
god2father <398290046@qq.com>
2026-09-01 00:26:53 +0800

    chore: bump addon version to 0.2.1

--- PvPIdiot/Core/Init.lua
+++ PvPIdiot/Core/Init.lua
@@ -1,7 +1,7 @@
 local ADDON_NAME, PvPIdiot = ...
 
 PvPIdiot.name = ADDON_NAME
-PvPIdiot.version = "0.2.0"
+PvPIdiot.version = "0.2.1"
 PvPIdiot.modules = PvPIdiot.modules or {}
 PvPIdiot.widgets = PvPIdiot.widgets or {}
 PvPIdiot.ui = PvPIdiot.ui or {}
